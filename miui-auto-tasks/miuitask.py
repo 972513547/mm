1966b5520cf9a1bdc4fc7f4256051a3f92629591
kokoa <48902166+972513547@users.noreply.github.com>
2024-01-02 11:29:29 +0800

    Add files via upload

---
+++ miui-auto-tasks/miuitask.py
@@ -0,0 +1,57 @@
+'''
+Date: 2023-11-13 20:29:19
+LastEditors: Night-stars-1 nujj1042633805@gmail.com
+LastEditTime: 2023-12-03 01:53:48
+'''
+# new Env("MIUI-Auto-Task") # pylint: disable=missing-module-docstring
+# cron 30 8 * * * miuitask.py
+
+import asyncio
+
+from utils.api.login import Login
+from utils.api.sign import BaseSign, CheckIn
+from utils.config import ConfigManager
+from utils.logger import InterceptHandler, log
+from utils.request import notify_me
+from utils.system_info import print_info
+from utils.utils import get_token
+
+_conf = ConfigManager.data_obj
+
+
+async def main():
+    """启动签到"""
+    print_info()
+    for account in _conf.accounts:
+        login_obj = Login(account)
+        if cookies := await login_obj.login():
+            sign_obj = BaseSign(cookies)
+            daily_tasks = await sign_obj.check_daily_tasks()
+            sign_task_obj = sign_obj.AVAILABLE_SIGNS  # 签到任务对象合集
+            for task in daily_tasks:
+                if not task.showType:
+                    log.info(f"开始执行{task.name}任务")
+                    if task_obj := sign_task_obj.get(task.name):  # 签到任务对象
+                        if getattr(account, task_obj.__name__):
+                            if task_obj == CheckIn:
+                                for _ in range(10):
+                                    if token := await get_token(cookies["cUserId"], use_2captcha=True):
+                                        break
+                                else:
+                                    for _ in range(10):
+                                        if token := await get_token(cookies["cUserId"], use_2captcha=False):
+                                            break
+                            else:
+                                token = None
+                            await task_obj(cookies, token).sign()
+                        else:
+                            log.info(f"任务{task.name}被禁用")
+                    else:
+                        log.error(f"未找到{task.name}任务")
+                else:
+                    log.info(f"{task.name}任务已完成")
+    notify_me(InterceptHandler.message)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ miui-auto-tasks/utils/api/login.py
@@ -0,0 +1,106 @@
+"""
+Date: 2023-11-12 14:05:06
+LastEditors: Night-stars-1 nujj1042633805@gmail.com
+LastEditTime: 2023-11-13 12:32:26
+"""
+from os import getenv
+from typing import Dict, Union
+
+import orjson
+
+from ..config import Account, write_plugin_data
+from ..data_model import LoginResultHandler
+from ..logger import log
+from ..request import get, post
+from .sign import BaseSign
+
+
+async def get_cookie(url: str) -> Union[Dict[str, str], bool]:
+    """获取社区 Cookie"""
+    try:
+        response = await get(url, follow_redirects=False)
+        log.debug(response.text)
+        return dict(response.cookies)
+    except Exception: # pylint: disable=broad-exception-caught
+        log.exception("社区获取 Cookie 失败")
+        return False
+
+
+class Login:
+    """登录类"""
+
+    def __init__(self, account: Account) -> None:
+        self.account = account
+        self.user_agent = account.user_agent
+        self.uid = account.uid
+        self.password = account.password
+        self.cookies = account.cookies
+
+    async def login(self) -> Union[Dict[str, str], bool]:
+        """登录小米账号"""
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Referer': 'https://account.xiaomi.com/fe/service/login/password?sid=miui_vip&qs=%253Fcallback%253Dhttp'
+                       '%25253A%25252F%25252Fapi.vip.miui.com%25252Fsts%25253Fsign%25253D4II4ABwZkiJzkd2YSkyEZukI4Ak'
+                       '%2525253D%252526followup%25253Dhttps%2525253A%2525252F%2525252Fapi.vip.miui.com%2525252Fpage'
+                       '%2525252Flogin%2525253FdestUrl%2525253Dhttps%252525253A%252525252F%252525252Fweb.vip.miui.com'
+                       '%252525252Fpage%252525252Finfo%252525252Fmio%252525252Fmio%252525252FinternalTest%252525253Fref'
+                       '%252525253Dhomepage%2526sid%253Dmiui_vip&callback=http%3A%2F%2Fapi.vip.miui.com%2Fsts%3Fsign'
+                       '%3D4II4ABwZkiJzkd2YSkyEZukI4Ak%253D%26followup%3Dhttps%253A%252F%252Fapi.vip.miui.com%252Fpage'
+                       '%252Flogin%253FdestUrl%253Dhttps%25253A%25252F%25252Fweb.vip.miui.com%25252Fpage%25252Finfo'
+                       '%25252Fmio%25252Fmio%25252FinternalTest%25253Fref%25253Dhomepage&_sign=L%2BdSQY6sjSQ%2FCRjJs4p'
+                       '%2BU1vNYLY%3D&serviceParam=%7B%22checkSafePhone%22%3Afalse%2C%22checkSafeAddress%22%3Afalse%2C'
+                       '%22lsrp_score%22%3A0.0%7D&showActiveX=false&theme=&needTheme=false&bizDeviceType=',
+            'User-Agent': str(self.user_agent),
+            'Origin': 'https://account.xiaomi.com',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Cookie': 'deviceId=; pass_ua=web; uLocale=zh_CN'
+        }
+        data = {
+            'bizDeviceType': '',
+            'needTheme': 'false',
+            'theme': '',
+            'showActiveX': 'false',
+            'serviceParam': '{"checkSafePhone":false,"checkSafeAddress":false,"lsrp_score":0.0}',
+            'callback': 'http://api.vip.miui.com/sts?sign=4II4ABwZkiJzkd2YSkyEZukI4Ak%3D&followup=https%3A%2F%2Fapi.vip'
+                        '.miui.com%2Fpage%2Flogin%3FdestUrl%3Dhttps%253A%252F%252Fweb.vip.miui.com%252Fpage%252Finfo'
+                        '%252Fmio%252Fmio%252FinternalTest%253Fref%253Dhomepage',
+            'qs': '%3Fcallback%3Dhttp%253A%252F%252Fapi.vip.miui.com%252Fsts%253Fsign%253D4II4ABwZkiJzkd2YSkyEZukI4Ak'
+                  '%25253D%2526followup%253Dhttps%25253A%25252F%25252Fapi.vip.miui.com%25252Fpage%25252Flogin'
+                  '%25253FdestUrl%25253Dhttps%2525253A%2525252F%2525252Fweb.vip.miui.com%2525252Fpage%2525252Finfo'
+                  '%2525252Fmio%2525252Fmio%2525252FinternalTest%2525253Fref%2525253Dhomepage%26sid%3Dmiui_vip',
+            'sid': 'miui_vip',
+            '_sign': 'L+dSQY6sjSQ/CRjJs4p+U1vNYLY=',
+            'user': str(self.uid),
+            'cc': '+86',
+            'hash': str(self.password),
+            '_json': 'true'
+        }
+        try:
+            repo_owner = getenv('GITHUB_REPOSITORY_OWNER')
+            if repo_owner not in [None, "0-8-4"]:
+                return False
+            if self.cookies != {} and await BaseSign(self.cookies).check_daily_tasks(nolog=True) != []:
+                log.info("Cookie有效，跳过登录")
+                return self.cookies
+            response = await post('https://account.xiaomi.com/pass/serviceLoginAuth2', headers=headers, data=data)
+            log.debug(response.text)
+            result = response.text.lstrip('&').lstrip('START').lstrip('&')
+            data = orjson.loads(result)  # pylint: disable=no-member
+            api_data = LoginResultHandler(data)
+            if api_data.success:
+                log.success('小米账号登录成功')
+                cookies = await get_cookie(api_data.location)
+                self.account.cookies = cookies
+                write_plugin_data()
+                return cookies
+            elif not api_data.pwd_wrong:
+                log.error(f'小米账号登录失败：{api_data.message}')
+            elif api_data.need_captcha:
+                log.error('当前账号需要短信验证码, 请尝试修改UA或设备ID')
+            else:
+                log.error('小米账号登录失败：用户名或密码不正确')
+            return False
+        except Exception: # pylint: disable=broad-exception-caught
+            log.exception("登录小米账号出错")
+            return False
